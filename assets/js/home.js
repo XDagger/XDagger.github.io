2a95b4b506d4b81769e1b0e43c3d08b49ef9be35
Jimmy <31631352+timlah@users.noreply.github.com>
2018-11-03 16:36:50 +0200

    Add QQ to community links

--- assets/js/home.js
+++ assets/js/home.js
@@ -44,7 +44,7 @@ jQuery(document).ready(function( $ ) {
     autoplay: true,
     dots: true,
     loop: true,
-    responsive: { 0: { items: 2 }, 768: { items: 4 }, 900: { items: 6 }
+    responsive: { 0: { items: 2 }, 768: { items: 4 }, 1100: { items: 7 }
     }
   });
 });
